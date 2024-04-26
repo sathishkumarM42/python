b7324220e3e16ca8d771bd8ed7b9701facf11b35
sathishkumarM42 <70696451+sathishkumarM42@users.noreply.github.com>
2024-04-26 16:48:12 +0100

    test

--- learnings/day_1/string_manuplation.py
+++ learnings/day_1/string_manuplation.py
@@ -1,23 +1,23 @@
 print("Swap the values")
-#a=input("enter the value of a \n")
-#b=input("enter the value of b \n")
-#c=a
-#a=b
-#b=c
+a=input("enter the value of a \n")
+b=input("enter the value of b \n")
+c=a
+a=b
+b=c
 # added extra line
 # Test Mac Git 2016, 2018,1999,2020, 1900
-year=2024
-leap_year=year%4
+#year=2024
+#leap_year=year%4
 
-if year%100:
-    if year%400:
-        if year%4:
-            print("Leap Year")
-else:
-   print("Non Leap Year")
+#if year%100:
+    #if year%400:
+       #if year%4:
+            #print("Leap Year")
+#else:
+   #print("Non Leap Year")
 
-#print("a: "+ a)
-#print("b: "+ b)
+print("a: "+ a)
+print("b: "+ b)
 
 
 
